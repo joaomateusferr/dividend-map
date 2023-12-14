8f59a5106778319d880b1aa9f10aba56c41cff8e
Joao Ferreira <joaomateusferr@gmail.com>
2023-12-14 12:21:09 -0300

    Automate deploy script

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -4,7 +4,7 @@ if [ $(id -u) -ne 0 ]; then #this screipt equire root privileges (root id is 0)
     echo 'No root privileges detected!'
     echo 'Please, run this script as root'
 else
-    apt-get install python3 python3-pip
+    apt-get -y install python3 python3-pip
     python3 -m pip install requests
     python3 -m pip install yfinance
 fi
